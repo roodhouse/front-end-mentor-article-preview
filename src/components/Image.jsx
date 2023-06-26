46a5666ce4d35fa4e25957d1788768aca0022e44
roodhouse <rughjm@gmail.com>
2023-06-26 13:01:54 -0500

    fixed issue

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,7 +4,9 @@ import Drawers from '../images/drawers.jpg'
 function Image() {
   return (
     <>
+    <div id='imageContainer' className='relative overflow-hidden h-[215px]'>
         <img src={Drawers} alt='Drawers' className='rounded-[10px_10px_0px_0px]'/>
+    </div>
     </>
   )
 }
